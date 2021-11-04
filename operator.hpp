83c9bbd3ada6dc6c05d580c017273a2f2e299d27
IngridAMorales <ingridmorales1016@gmail.com>
2021-11-03 19:19:03 -0700

    Mult class is working on all edge cases

--- operator.hpp
+++ operator.hpp
@@ -2,11 +2,53 @@
 #define __OPERATOR_HPP__
 
 #include "base.hpp"
-#include "op.hpp"
 
+class SevenOpMock: public Base {
+    public:
+        SevenOpMock() { }
 
+        virtual double evaluate() { return 7.5; }
+        virtual std::string stringify() { return "7.5"; }
+};
 
+class FourOpMock: public Base {
+    public:
+   	FourOpMock() { }
 
+        virtual double evaluate() { return 4.5; }
+        virtual std::string stringify() { return "4.5"; }
+};
+class ZeroOpMock: public Base {
+   public: 
+	ZeroOpMock() { }
+
+	virtual double evaluate() { return 0.0; }
+        virtual std::string stringify() { return "0.0"; }
+}; 
+class NegOpMock: public Base {
+   public: 
+	NegOpMock() { }
+
+	virtual double evaluate() { return -7.5; }
+	virtual std::string stringify() { return "-7.5"; }
+};
+class Mult : public Base { 
+	private: 
+	   Base* left; 
+	   Base* right; 
+	public: 
+	   Mult(Base* op1, Base* op2) : Base() {
+		left = op1; 
+	        right = op2; 
+	   }
+
+	   virtual double evaluate() {
+	   	return this->left->evaluate() * this->right->evaluate(); 
+	   }
+	   virtual std::string stringify() {
+		return "("+ this->left->stringify()+ "*"+ this->right->stringify()+ ")";  
+	   }
+};
 
 
 
---
+++ operator_test.hpp
@@ -0,0 +1,63 @@
+#ifndef __OPERATOR_TEST_HPP__
+#define __OPERATOR_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "operator.hpp"
+
+
+TEST(OperatorTest, OpEvaluateNonZero) {
+    Base* seven = new SevenOpMock();
+    Base* four = new FourOpMock();
+    Mult* test = new Mult(seven, four); 
+
+    EXPECT_DOUBLE_EQ(test->evaluate(), 33.75);
+}
+
+TEST(OperatorTest, OpEvaluateZero) {
+    Base* seven = new SevenOpMock(); 
+    Base* zero = new ZeroOpMock(); 
+    Mult* test = new Mult(seven, zero); 
+
+    EXPECT_DOUBLE_EQ(test->evaluate(), 0.0); 
+} 
+
+TEST(OperatorTest, OpEvaluateNeg) {
+    Base* seven = new NegOpMock(); 
+    Base* four = new FourOpMock(); 
+    Mult* test = new Mult(seven, four); 
+
+    EXPECT_DOUBLE_EQ(test->evaluate(), -33.75); 
+}
+ 
+TEST(OperatorTest, OpStringify) {
+    Base* seven = new SevenOpMock(); 
+    Base* four = new FourOpMock(); 
+    Mult* test = new Mult(seven, four); 
+
+    EXPECT_EQ(test->stringify(), "(7.5*4.5)"); 
+}
+
+TEST(OperatorTest, OpStringifyZero) {
+    Base* seven = new SevenOpMock(); 
+    Base* zero = new ZeroOpMock(); 
+    Mult* test = new Mult(seven, zero); 
+
+    EXPECT_EQ(test->stringify(), "(7.5*0.0)"); 
+}
+
+TEST(OperatorTest, OpStringifyNeg) {
+   Base* seven = new NegOpMock(); 
+   Base* four = new FourOpMock(); 
+   Mult* test = new Mult(seven, four); 
+
+   EXPECT_EQ(test->stringify(), "(-7.5*4.5)"); 
+}
+
+
+
+
+
+
+
+
+#endif
